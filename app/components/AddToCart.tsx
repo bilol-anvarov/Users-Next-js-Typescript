1997f8efa91082069939f0d9289a559fa45eac16
Билол <126669638+bilol-anvarov@users.noreply.github.com>
2024-08-17 14:23:37 +0500

    first commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <button onClick={()=>{console.log('Click!')}}>Add to Cart</button>
+  )
+}
+
+export default AddToCart
